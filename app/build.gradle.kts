1a0829fc15f47897bfd7b33659f75b56eda0f98a
vipe1375 <100449104+vipe1375@users.noreply.github.com>
2025-02-08 13:55:48 +0100

    update build files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-    alias(libs.plugins.google.gms.google.services)
     alias(libs.plugins.compose.compiler)
 
     id("com.google.devtools.ksp") version "2.0.0-1.0.21"
@@ -64,7 +63,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.firebase.database)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.runtime.livedata)
     testImplementation(libs.junit)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,11 +17,13 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
     }
+    /*
     versionCatalogs {
         create("libs") {
             from(files("gradle/libs.versions.toml"))
         }
     }
+    */
 }
 
 rootProject.name = "KordsJetpack"
