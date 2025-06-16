a32c0648edb3484ee98cdf91fb89db4e5a5aaf3b
vipe1375 <100449104+vipe1375@users.noreply.github.com>
2025-06-16 19:37:17 +0200

    fix fastlane metadata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.vipedev.kords"
         minSdk = 24
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.2.5"
+        versionCode = 8
+        versionName = "1.2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resourceConfigurations.plus(listOf("en", "fr"))
