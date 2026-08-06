b4e4edf85439c249af5301de5c8fc898f85b6725
Victor Pezennec--Deutsch <victor.pez1375@gmail.com>
2026-08-06 18:58:32 -0400

    fix gcem import

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,18 +98,6 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
     //implementation(libs.kotlinx.serialization.json)
     implementation(libs.gson)
-    // implementation("dev.kotlinds:fluidsynth-kmp:1.1.0")
-/*
-
-    // Import the BoM for the Firebase platform
-    implementation(platform(libs.firebase.bom))
-
-    // Add the dependency for the Realtime Database library
-    // When using the BoM, you don't specify versions in Firebase library dependencies
-    implementation(libs.google.firebase.database)
-*/
-
-
 
     // ViewModel
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
